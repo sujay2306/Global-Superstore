7a5fdd706df12259cc833add26a4f06d1284a361
sujay2306 <sujayks007@gmail.com>
2023-01-23 07:42:58 +0530

    added inventory mapping and datastore for categories

---
+++ src/main/java/com/ltp/globalsuperstore/StoreController.java
@@ -0,0 +1,18 @@
+package com.ltp.globalsuperstore;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public  class StoreController {
+
+    @GetMapping("/")
+    public String getForm(Model model){
+        model.addAttribute("categories", Constants.CATEGORIES);
+        return "form";
+    }
+    @GetMapping("/inventory")
+    public String inventory(){
+        return "inventory";
+    }
+}
